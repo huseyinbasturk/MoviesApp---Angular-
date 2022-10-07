2aafab081dd6c3604c04e66d14a039ab5fea2a01
huseyinbasturk <hb.basturk41@gmail.com>
2022-10-07 09:24:52 +0200

    first commit

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
+import { Item } from './item';
+@Component({
+  selector: 'item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+  @Input() itemData: Item | null = null;
+  @Input() isMovie: any = true;
+
+  imagesSizes = IMAGES_SIZES;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/components/items-banner/items-banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../models/movie';
+import { Item } from '../item/item';
+
+@Component({
+  selector: 'items-banner',
+  templateUrl: './items-banner.component.html',
+  styleUrls: ['./items-banner.component.scss']
+})
+export class ItemsBannerComponent {
+  @Input() items: Item[] = [];
+  @Input() title: string = '';
+  @Input() isMovie: any = true;
+}
